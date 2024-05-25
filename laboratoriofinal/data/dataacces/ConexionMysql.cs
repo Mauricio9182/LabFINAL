b6ba8d008e44e3ee243d7ac981cf1cecf9942a8a
mauricio9182 <pablocarrilopb0199@gmail.com>
2024-05-25 10:56:22 -0600

    Agregar archivos de proyecto.

---
+++ laboratoriofinal/data/dataacces/ConexionMysql.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratoriofinal.data.dataacces
+{
+    internal class ConexionMysql
+    {
+        string connectionString = "server=localhost;database=LaboratorioCRUD;user=root;password=Soynoob9182";
+        MySqlConnection connection;
+
+
+        //constructor
+        public ConexionMysql()
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+    }
+}
---
+++ laboratoriofinal/data/dataacces/usuarios.cs
@@ -0,0 +1,43 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratoriofinal.data.dataacces
+{
+    internal class usuarios
+    {
+        public int ID { get; set; }
+        public string Nombreconsola { get; set; }
+        public string compañia { get; set; }
+        public DateTime Fechalanzamiento { get; set; }
+        public int generacions { get; set; }
+        public usuarios() { }
+
+        public usuarios(int ID, string Nombreconsola, string compañia, DateTime Fechalanzamiento, int generacions)
+        {
+            ID = ID;
+            Nombreconsola = Nombreconsola;
+            compañia = compañia;
+            Fechalanzamiento = Fechalanzamiento;
+            generacions = generacions;
+
+        }
+        public int Obtenerfechasalida()
+        {
+            int fecha = DateTime.Now.Year - Fechalanzamiento.Year;
+            if (DateTime.Now.DayOfYear < Fechalanzamiento.DayOfYear)
+            {
+                fecha--;
+            }
+            return fecha;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Nombreconsola: {Nombreconsola} {generacions}, fecha de nacimiento: {Fechalanzamiento.ToShortDateString()}";
+        }
+        }
+    }
+}
